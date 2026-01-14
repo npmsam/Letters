3a76049673ffef96a9e01735eb7de97e81a80d6a
Sampreet Singh <preet.samsam+github@gmail.com>
2026-01-14 19:38:39 +0530

    print enable disable messages

--- src/main/java/com/sampreet/letters/Letters.java
+++ src/main/java/com/sampreet/letters/Letters.java
@@ -7,11 +7,47 @@ public final class Letters extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        // Save default config.yml to plugin folder
+        saveDefaultConfig();
 
+        // Check whether plugin is enabled or disabled in config.yml
+        if (!getConfig().getBoolean("enabled", true)) {
+            // Retrieve message from config and return if message is null
+            String disabledMessage = getConfig().getString("messages.system.checks.plugin-disabled");
+            if (disabledMessage != null && !disabledMessage.trim().isEmpty()) {
+                // Insert current plugin version into placeholder
+                disabledMessage = disabledMessage.replace("%plugin_version%", getDescription().getVersion());
+
+                // Log that the plugin has successfully loaded and is ready
+                getLogger().info(disabledMessage);
+            }
+
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        // Retrieve message from config and return if message is null
+        String enableMessage = getConfig().getString("messages.system.lifecycle.enable");
+        if (enableMessage == null || enableMessage.trim().isEmpty()) return;
+
+        // Insert current plugin version into placeholder
+        enableMessage = enableMessage.replace("%plugin_version%", getDescription().getVersion());
+
+        // Log that the plugin has successfully loaded and is ready
+        getLogger().info(enableMessage);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        // Retrieve message from config and return if message is null
+        String disableMessage = getConfig().getString("messages.system.lifecycle.disable");
+        if (disableMessage == null || disableMessage.trim().isEmpty()) return;
+
+        // Insert current plugin version into placeholder
+        disableMessage = disableMessage.replace("%plugin_version%", getDescription().getVersion());
+
+        // Log that the plugin has been disabled
+        getLogger().info(disableMessage);
     }
 }
