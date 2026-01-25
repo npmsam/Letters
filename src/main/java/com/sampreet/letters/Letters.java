cda1d8dad99159d63c5bc72f4da412e6b5d05d31
Sampreet Singh <preet.samsam+github@gmail.com>
2026-01-26 03:36:04 +0530

    print enable disable messages

--- src/main/java/com/sampreet/letters/Letters.java
+++ src/main/java/com/sampreet/letters/Letters.java
@@ -6,12 +6,27 @@ public final class Letters extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
 
+        if (!getConfig().getBoolean("enabled", true)) {
+            logMessage("system_messages.status.disabled");
+
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        logMessage("system_messages.lifecycle.enable");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        logMessage("system_messages.lifecycle.disable");
+    }
+
+    private void logMessage(String path) {
+        String message = getConfig().getString(path);
+        if (message == null || message.trim().isEmpty()) return;
+        message = message.replace("<plugin_version>", getDescription().getVersion());
+        getLogger().info(message);
     }
 }
